f4e6e761231ccc52a97ddc4feee57ef9bc31c825
Mtaaha94 <muhammadtaaha.94@gmail.com>
2021-04-02 15:23:09 +0500

    first commit

---
+++ front/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import navbar from './components/navbar'
+import Card from './components/card'
+//import drawer from './components/drawer'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <navbar/>
+      <Card/>
+    
+    </div>
+    
+  );
+}
+
+export default App;
